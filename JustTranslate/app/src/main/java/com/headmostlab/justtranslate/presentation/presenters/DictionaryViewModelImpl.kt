1d62c33edec0eb59760f7a1675f22631782d13a8
Харапаев Владимир <vharapaev@rbc.ru>
2021-09-09 23:01:41 +0300

    hw4

--- JustTranslate/app/src/main/java/com/headmostlab/justtranslate/presentation/presenters/DictionaryViewModelImpl.kt
+++ JustTranslate/app/src/main/java/com/headmostlab/justtranslate/presentation/presenters/DictionaryViewModelImpl.kt
@@ -7,31 +7,20 @@ import androidx.lifecycle.viewModelScope
 import com.headmostlab.justtranslate.domain.entities.Translations
 import com.headmostlab.justtranslate.domain.interactors.DictionaryInteractor
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class DictionaryViewModelImpl constructor(
     private val dictionaryInteractor: DictionaryInteractor
 ) : ViewModel() {
 
-    private val translations = MutableLiveData<List<Translations>>()
+    private val _translations = MutableLiveData<List<Translations>>()
+    val translations: LiveData<List<Translations>> = _translations
 
-    private var job: Job? = null
+    private val _defaultWord = MutableLiveData("word")
+    val defaultWord: LiveData<String> = _defaultWord
 
-    fun search(word: String): LiveData<List<Translations>> {
-        job?.cancel()
-        job = viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                try {
-                    translations.postValue(emptyList())
-                    val found = dictionaryInteractor.getTranslations(word)
-                    translations.postValue(found)
-                } catch (e: Throwable) {
-                    e.printStackTrace()
-                }
-            }
-        }
-        return translations
+    fun search(word: String) = viewModelScope.launch(Dispatchers.IO) {
+        _translations.postValue(dictionaryInteractor.getTranslations(word))
     }
+
 }
