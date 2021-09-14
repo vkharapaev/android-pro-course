5eee03e2a5b665823f39585a1d40ca1cb177c9f2
Харапаев Владимир <vharapaev@rbc.ru>
2021-09-14 19:54:43 +0300

    hw5

---
+++ JustTranslate/buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,5 @@
+object Room {
+    const val runtime = "androidx.room:room-runtime:${Versions.room}"
+    const val compiler = "androidx.room:room-compiler:${Versions.room}"
+    const val ktx = "androidx.room:room-ktx:${Versions.room}"
+}
